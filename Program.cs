78f07ae729dafab6066481c5dafcaf0fe42ca63e
Alvaro11G <agarcia11746@gmail.com>
2022-08-26 22:06:29 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    class Program
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            double answer;
+
+            answer = Math.PI * radius * radius;
+
+            return answer;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double answer2 = AreaOfCircle(radius);
+
+            Console.WriteLine("The area of a circle with radius of " + radius + " is " + answer2);
+
+        }
+    }
+}
